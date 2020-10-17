7a39ab179eb7e3db507b1f3fa1e45d14058c6785
James Ji <1stlulu@gmail.com>
2020-10-18 03:26:35 +0900

    Enable video download

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 # define ffmpeg base
 FROM jrottenberg/ffmpeg:4.1-alpine AS ffmpeg-base
 
+FROM alpine:3.11 AS transfer-base
+RUN apk add --no-cache wget tar && \
+        wget -q https://github.com/Mikubill/transfer/releases/download/v0.4.7/transfer_0.4.7_linux_amd64.tar.gz && \
+        tar -xzvf transfer_0.4.7_linux_amd64.tar.gz && \
+        chmod +x transfer
+
 FROM alpine:3.11 AS runtime-base
 # get Python, PIP, you-get
 RUN apk add --no-cache python3 \
@@ -15,6 +21,8 @@ RUN apk add --no-cache python3 \
 RUN apk add --no-cache --update libgcc libstdc++ ca-certificates libcrypto1.1 libssl1.1 libgomp expat git
 # copy ffmpeg libraries from ffmpeg base 
 COPY --from=ffmpeg-base /usr/local /usr/local
+# copy transfer from transfer-base
+COPY --from=transfer-base transfer /usr/local/bin/transfer
 ENV LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64
 # run script
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,31 @@
 #!/bin/sh -l
 
-echo "::set-output name=transfer_urls::$bili_video_url"
+bili_video_url=$1
+proxy_url=$2
+bili_cookie=$3
+
+you_get_cmd="you-get"
+if [[ -n "$proxy_url" ]]; then
+    you_get_cmd="$you_get_cmd -y $proxy_url $bili_video_url"
+else
+    you_get_cmd="$you_get_cmd $bili_video_url"
+fi
+
+echo "Fetching Bilibili video with you-get"
+echo "$you_get_cmd"
+you_get_output=$(eval $you_get_cmd | grep "Merging video parts... Merged into " | sed "s/Merging video parts... Merged into //")
+if [[ -z "$you_get_output" ]]; then 
+    echo >&2 "Failed to download $bili_video_url with you-get"
+    exit 1
+fi
+if ffprobe "$you_get_output" 2>&1 | grep "Video: hevc"; then
+    echo "Optimizing hevc video for Apple devices: adding hvc1 tag"
+    apple_optimized_output=$(echo "$you_get_output" | sed "s/.mp4/.hvc1-tagged.mp4/")
+    ffmpeg -i "$you_get_output" -c copy -tag:v hvc1 -hide_banner -loglevel panic "$apple_optimized_output" 
+else 
+    apple_optimized_output="$you_get_output"
+fi
+
+echo $bili_cookie
+
+echo "::set-output name=transfer_urls::$apple_optimized_output"
